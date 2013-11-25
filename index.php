a047e3537a72ba25a79890ca9ce9b2315db20220
Aankita <aankita.mukherjee@gmail.com>
2013-11-25 15:18:41 +0800

    initial version page 1

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Getting started with using Ohanae">
+		<meta charset="utf-8">
+
+		<link type="text/css" rel="stylesheet" href="stylesheet_tut.css">
+		<link type="text/css" rel="stylesheet" href="common/header.css">
+		<link type="text/css" rel="stylesheet" href="common/footer.css">
+		
+		<script type="text/javascript" src="resources/plugins/jquery-1.10.0.js"></script> 
+        <script type="text/javascript" src="resources/scripts/jquery.client.min.js"></script>
+        <script type="text/javascript" src="resources/scripts/detectdevice.js"></script>
+        <script type="text/javascript" src="resources/scripts/jquery.js"></script>
+		
+		<title>Ohanae: Tutorial</title>
+	</head>
+
+<body>
+
+	<a id="top"></a>
+	<div id="backtotop">
+		<a href="#top">Back to top</a>
+	</div>
+	
+	<div id="pg4" class="content">
+		<h1>Getting Started</h1>
+		<p>Thank you for using Ohanae! 
+		<br/><br/>
+		Spend a few minutes going through this guide to learn 
+		some basics of Ohanae. We will introduce you to a 
+		number of useful functions that will help you use 
+		our product to its fullest. 
+		</br></br>
+		Before getting started, here are a few terms 
+		that will be used throughout this tutorial:
+		<br/></br>
+		Ohanae Account: 
+		Your account registered with Ohanae including a 
+		username and passphrase or password.
+		<br/><br/>
+		Private Files:
+		Your personal files with sensitive information. These
+		are stored in designated folders, or in ordinary folders 
+		in an encrypted form.
+		<br/><br/>
+		Secure Folder:
+		Designated folders and drive, in which all files are private.
+		</p>
+		<ul>
+			<li>
+			Ohanae Account: 
+			Your account registered with Ohanae including a 
+			username and passphrase or password.
+			</li>
+			
+			<li>Private Files:
+			Your personal files with sensitive information. These
+			are stored in designated folders, or in ordinary folders 
+			in an encrypted form.
+			</li>
+			
+			<li>Secure Folder:
+			Designated folders and drive, in which all files are private.
+			</br>
+			</li>
+			
+	<!--		<li>
+			<p>1-Tap Login:
+			1-Tap Login allows you to log into multiple accounts 
+			for different services only through 1 click. Ohanae manages 
+			all your passwords. You simply need to select your username.
+			</p>
+			</li>
+			
+			<li>Secure Workplace:
+			Ohanae Secure Workplace ensures your secure 
+			information is not compromised. Secure Workplace enables you to 
+			work conveniently with your files, while ensuring that you do 
+			not leave work file with sensitive information unencrypted on a device. 
+			</br>
+			</li>
+			
+			<li>Secure Drive:
+			Ohanae Secure Drive enables you to coveniently store data while 
+			protecting you from security threats to private data. It encrypts 
+			and stores your data locally as well as on the cloud.
+			</br>
+			</li>
+			
+			<li>Secure File Sharing Client:
+			The online website that manages access control for encrypted 
+			files and folders, allowing users to add new groups and members 
+			who can view private documents.
+			</br>
+			</li>		-->
+		</ul>
+	</div>
+
+	<div id="navBar">
+		<div id="links">
+			<a href="http://www.google.com">Windows</a>
+			<a href="#information_protection">Mac OS</a>
+			<a href="#cloud_compliance">iOS</a>
+			<a href="#cloud_compliance">Android</a>
+			<a href="#cloud_compliance">Windows Phone</a>
+			<a href="#cloud_compliance">Windows RT</a>
+		</div>
+	</div>	
+	
+
+</body>
+</html>
